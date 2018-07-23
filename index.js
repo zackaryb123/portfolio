777b27d6e569c70fec3df000128a9394f33a2cad
Zack Blaylock <zackblaylock@gmail.com>
2018-07-23 13:00:14 -0400

    updated portfolio

--- index.js
+++ index.js
@@ -1,54 +1,55 @@
-// function onNavButton() {
-//     $('.nav').on('click','button', function (event) {
-//         const target = $(event.currentTarget).parent().parent().children()[1].id;
-//         switch (target) {
-//             case 'Projects':
-//                 if($('#Projects').hasClass('active')) {
-//                     $('#Projects').removeClass('active')
-//                     $('#Projects').attr('hidden', true);
-//                 }
-//                 else {
-//                     $('#Projects').addClass('active')
-//                     $('#Projects').attr('hidden', false);
-//                 }
-//                 break;
-//             case 'Skills':
-//                 if($('#Skills').hasClass('active')) {
-//                     $('#Skills').removeClass('active')
-//                     $('#Skills').attr('hidden', true);
-//                 }
-//                 else {
-//                     $('#Skills').addClass('active')
-//                     $('#Skills').attr('hidden', false);
-//                 }
-//                 break;
-//             case 'Tech':
-//                 if($('#Tech').hasClass('active')) {
-//                     $('#Tech').removeClass('active')
-//                     $('#Tech').attr('hidden', true);
-//                 }
-//                 else {
-//                     $('#Tech').addClass('active')
-//                     $('#Tech').attr('hidden', false);
-//                 }
-//                 break;
-//         }
-//     });
-// }
-
 function onDisplayPage() {
     $('.nav').on('click', 'li', function(event) {
-      const currentPage = $('main').find('section:visible');
-      const target = $(event.currentTarget).children().attr('href');
-      if($(target).hasClass('active')) {
-        return;
-      }else{
-        $(currentPage).removeClass('active');
-        $(currentPage).attr('hidden', true);
-        $(target).addClass('active');
-        $(target).attr('hidden', false);
-      }
-    })
+      const pageHeader = $('.page-header');
+          // Wait for page swap
+          $.when( pageSwap(event) )
+            .done( function( d ) {
+              resetHeader(pageHeader);
+            })
+            .fail( function( err ) {
+              console.log( err, 'page swap failed.' );
+            })
+            // Wait for reset
+            .then( function() {
+              $.when( resetHeader(pageHeader) )
+                .done( function( d ) {
+                  annimateHeader(pageHeader);
+                } )
+                .fail( function( err ) {
+                  console.log( err, 'Reset failed.' );
+                })
+            });
+        });
+}
+
+function pageSwap(event) {
+  const currentPage = $('main').find('section:visible');
+  const target = $(event.currentTarget).children().attr('href');
+  if($(target).hasClass('active')) {
+    return;
+  }else{
+    // Switch Page
+    $(currentPage).removeClass('active');
+    $(currentPage).attr('hidden', true);
+    $(target).addClass('active');
+    $(target).attr('hidden', false);
+  }
+}
+
+function resetHeader(pageHeader) {
+  pageHeader.promise().done(function() {
+    const pageHeader = $('.page-header');
+    pageHeader.css('transition', '0s');
+    pageHeader.css('transform', 'translate(0px, 1rem)');
+  });
+}
+
+function annimateHeader(pageHeader) {
+  pageHeader.promise().done(function () {
+    const pageHeader = $('.page-header');
+    pageHeader.css('transition', '1s ease-in-out' );
+    pageHeader.css('transform', 'translate(0px, -5.5rem)');
+  });
 }
 
 function onCollapseNavBar() {
@@ -71,26 +72,9 @@ function onCollapseNavBar() {
     });
 }
 
-function onHoverBubbleTxt() {
-    $('.bubble').hover(
-      function(event) {
-        const tooltip = $(event.currentTarget).find('.tooltip');
-        tooltip.attr('hidden', false);
-        const arrow = $(event.currentTarget).find('.arrow-down');
-        arrow.attr('hidden', false);
-        const txt  = $(event.currentTarget).find('.bubble-txt');
-        txt.attr('hidden', true);
-    }, function(event){
-        const tooltip = $(event.currentTarget).find('.tooltip');
-        tooltip.attr('hidden', true);
-        const arrow = $(event.currentTarget).find('.arrow-down');
-        arrow.attr('hidden', true);
-        const txt  = $(event.currentTarget).find('.bubble-txt');
-        txt.attr('hidden', false);
-      }
-    )
+function handleFunction(){
+  onDisplayPage();
+  onCollapseNavBar();
 }
 
-$(onDisplayPage);
-$(onCollapseNavBar);
-$(onHoverBubbleTxt);
+$(handleFunction);
